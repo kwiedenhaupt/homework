9c7611184f97978dcbcec70bdcd3f01e95badfff
kwiedenhaupt <111075855+kwiedenhaupt@users.noreply.github.com>
2022-08-14 20:38:25 +0300

    Update homework.js

--- homework.js
+++ homework.js
@@ -132,7 +132,7 @@ function vowelsIn(string2) {
 }
 console.log(vowelsIn('doggie'));
 
-//10* alternate
+//* 10 alternate
 function charactersIn(string3) {
     let counter1=0
     for (let i=0; i<string3.length; i++) {
@@ -141,3 +141,46 @@ function charactersIn(string3) {
     return counter1
 }
 console.log(charactersIn('doggie'));
+
+//* react homework
+
+//* My Button
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+export default function App() {
+  return (
+  <div> 
+    <h1> Welcome to my app </h1>
+    <MyButton />
+  </div>
+  );
+}
+
+//* Hedy Lamarr
+
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+export default function Profile () {
+  return (
+    <>
+      <h1> {user.name} </h1>
+      <img 
+        className= "avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
+    </>
+  );
+}

519cd575f04f3dccc29c23a496f1e39ed0b8b0bf
kwiedenhaupt <111075855+kwiedenhaupt@users.noreply.github.com>
2022-08-15 13:30:09 +0300

    Update homework.js

--- homework.js
+++ homework.js
@@ -200,7 +200,7 @@ return (
 );
 
 //* conditional ? operator
-<div>
+<div>'
   {isLoggedIn ? (
   <AdminPanel />
   ) : (
@@ -212,3 +212,27 @@ return (
 //* using &&
 <div> {<isLoggedIn /> && <AdminPanel />}
 </div>
+
+//* lists
+const products = [
+  {title: 'Cabbage', isFruit: false, id: 1 },
+  {title: 'Garlic', isFruit: false, id: 2 },
+  {title: 'Apple', isFruit: true, id: 3 },
+  ];
+   
+export default function ShoppingList {
+  const listItems = products.map(product =>
+    <li
+    key = {product.id}
+    style = {{
+            color: product.isFruit ? 'magenta' : 'dark green'
+           }}
+     />
+        {product.title}
+    </li>
+   );
+
+  return (
+    <ul>{list.items)</ul>
+   );
+}

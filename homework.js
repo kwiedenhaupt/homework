28a4d2e53533e35a81e7a2f69ad0aad3e8673847
kwiedenhaupt <111075855+kwiedenhaupt@users.noreply.github.com>
2022-08-15 08:28:50 -0500

    Update homework.js

--- homework.js
+++ homework.js
@@ -294,3 +294,24 @@ export function Profile() {
     />
   );
 }
+
+//changing html to jsx
+
+export default function TodoList() {
+  return (
+    // This doesn't quite work!
+    <div>
+    	<h1>Hedy Lamarr's Todos</h1>
+    	<img 
+      		src="https://i.imgur.com/yXOvdOSs.jpg" 
+      		alt="Hedy Lamarr" 
+      		class="photo"
+    	/>
+    	<ul>
+      		<li>Invent new traffic lights</li>
+      		<li>Rehearse a movie scene</li>
+      		<li>Improve the spectrum technology</li>
+    	</ul>
+      </div>
+  );
+}
